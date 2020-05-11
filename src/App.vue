a07588cfe4bc8a5cbbf7d26d2b52f71bc15a308c
Rohanmapari <Rohanmapari@users.noreply.github.com>
2020-05-11 14:58:23 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="small-container">
+    <h1></h1>
+	<hello-world :myvalue="myValue" />
+	<hello-world @add:myvalue="addmyvalue" />
+
+<input type="text" v-model="myValue"/>
+<p><button type="button" v-on:click="alertMe(myValue)">Submit</button></p>
+    <hello-world />
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '@/components/HelloWorld.vue'
+
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+	},
+data(){
+	return{
+	name: 'User',
+	myValue: ''
+	}
+	
+},
+methods: {
+  addmyvalue(myvalue) {
+    if(isNaN(myvalue)){
+					alert("Please Enter Number");
+				}else{
+					alert("Cube of  " + myvalue +"  is " + myvalue * myvalue *myvalue );
+				}
+				
+			}
+  }
+}
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="hello-world">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello-world',
+    props: {
+    myvalue: Number,
+  },
+  methods: {
+          alertme(){
+              this.$emit('add:myvalue', this.myvalue)
+
+
+          }
+
+
+  }
+			
+			
+			
+
+  }
+
+</script>
+
+<style scoped></style>
